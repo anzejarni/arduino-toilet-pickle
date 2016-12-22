fc8e7a036e7919f3057bc415ca2f23a260dc008d
Anze <anze.jarni@codeart.si>
2016-12-22 19:38:17 +0100

    Adds code for transmitter and receiver

---
+++ receiver/receiver.ino
@@ -0,0 +1,126 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+#include <SPI.h>
+#include <VirtualWire.h>
+
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     10
+#define TFT_RST    9 
+#define TFT_DC     8
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+const uint8_t receive_pin = 4;
+const uint8_t display_power_pin = 7;
+uint8_t last_drawn_state = -1;
+unsigned long last_update = 0;
+
+void setup()
+{
+  pinMode(display_power_pin, OUTPUT);
+  digitalWrite(display_power_pin, HIGH);
+  
+  Serial.begin(9600);
+
+  // Use this initializer if you're using a 1.8" TFT
+  tft.initR(INITR_BLACKTAB); 
+  tft.setRotation(tft.getRotation()+1);
+
+  // Initialise the IO and ISR
+  vw_set_rx_pin(receive_pin);
+  vw_set_ptt_inverted(true);
+  vw_setup(128);	 // Bits per sec
+  vw_rx_start();       // Start the receiver PLL running
+  Serial.print("Serial");
+
+  drawNoData();
+}
+
+void drawOccupied()
+{
+  // large block of text
+  tft.fillScreen(ST7735_RED);  
+
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(3);
+  tft.setCursor(10, 50);
+  tft.println("ZASEDENO");
+}
+
+void drawFree()
+{
+  // large block of text
+  tft.fillScreen(ST7735_GREEN);  
+
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(3);
+  tft.setCursor(25, 50);
+  tft.println("PROSTO");
+}
+
+void drawNoData()
+{
+  // large block of text
+  tft.fillScreen(ST7735_BLACK);  
+
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(2.5);
+  tft.setCursor(20, 55);
+  tft.println("NI PODATKA");
+}
+
+void drawWrongData()
+{
+  // large block of text
+  tft.fillScreen(ST7735_BLACK);  
+
+  tft.setTextColor(ST7735_WHITE);
+  tft.setTextSize(2);
+  tft.setCursor(40, 55);
+  tft.println("NAPACEN");
+  tft.setCursor(40, 70);
+  tft.println("PODATEK");
+}
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    if (buf[0] != last_drawn_state){
+      switch (buf[0]) {
+        default:
+          drawWrongData();
+        case 0:
+          drawFree();
+          break;
+        case 1:
+          drawOccupied();
+          break;
+      }
+      
+      last_drawn_state = buf[0];
+    }
+
+    last_update = millis();
+
+    /*
+    for (i = 0; i < buflen; i++)
+    {                                                  
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
+    }
+    Serial.println();
+    */
+  }
+
+  // If no transmission has been accepted in the last 40 seconds, dispay NO DATA
+  if (last_drawn_state != -1 && millis() > last_update + 40000) {
+      drawNoData();
+      last_drawn_state = -1;
+  }
+
+}
---
+++ transmitter/transmitter.ino
@@ -0,0 +1,52 @@
+#include <VirtualWire.h>
+
+const int transmit_pin = 4;
+const int lightsensor_pin = A0;
+
+void setup()
+{
+  // Initialize light resistor
+  pinMode(lightsensor_pin, INPUT);
+    
+  // Initialise the IO and ISR
+  vw_set_tx_pin(transmit_pin);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(128);       // Bits per sec
+  Serial.begin(9600);
+}
+
+boolean isDark()
+{
+  int light_value = analogRead(lightsensor_pin);
+  if (light_value < 280) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+void sendFree()
+{
+  uint8_t code = 0;
+  vw_send(&code, 1);
+  vw_wait_tx();
+}
+
+
+void sendOccupied()
+{
+  uint8_t code = 1;
+  vw_send(&code, 1);
+  vw_wait_tx();
+}
+
+void loop()
+{
+  if (isDark()) {
+   sendFree(); 
+  } else {
+   sendOccupied();
+  }
+
+  delay(1000);
+}
